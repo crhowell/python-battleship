d07e462b22fa52713ff806b16554daf59e734cd3
Chris H <crhowell1@gmail.com>
2016-07-29 17:32:35 -0700

    Update code cleanup and comments for game.py

--- game/game.py
+++ game/game.py
@@ -20,9 +20,12 @@ class Game:
         self.game_over = False
 
     def play(self):
+        """Game Loop
+
+        ** Needs Refactoring **
+        """
         print('Game Running')
         board = self.enemy_board()
-        i = 0
         while not self.game_over:
             self.print_game_board()
             move = self.get_player_move()
@@ -54,18 +57,20 @@ class Game:
         print('Winner is {}'.format(self.players[1]['player']))
 
     def update_boards(self, cell, token):
+        """Updates current and opposing player's boards."""
         self.players[1]['board'].place_token(cell, token)
         self.players[0]['moves'].place_token(cell, token)
 
-
     def current_board(self):
+        """Return the current player board."""
         return self.players[0]['board']
 
     def enemy_board(self):
-        enemy = self.players[1]
-        return enemy['board']
+        """Return the opposing players board."""
+        return self.players[1]['board']
 
     def sunk_ship(self):
+        """Increment player ship sunk count by 1."""
         self.players[1]['sunk'] += 1
 
     def get_player_move(self):
@@ -85,6 +90,7 @@ class Game:
         self.players.append(self.players.pop(0))
 
     def place_ships(self):
+        """Placement of all Game Players ships."""
         for player in self.players:
             self.prompt_to_continue(
                 '\n{} you will now place your ships.\n'.format(player['player'])
@@ -101,22 +107,21 @@ class Game:
                     # self.clear_screen()
                     placed = board.place_ship(ship, move, angle)
 
-    def mask_row(self, row):
-        mask_pieces = [
-            Board.INDICATORS['vship'],
-            Board.INDICATORS['hship']
-        ]
-        new_row = [cell if cell not in mask_pieces else
-                   Board.INDICATORS['empty'] for cell in row]
-        return new_row
-
     def get_ship(self, loc):
+        """Return a ship, by a given location.
+
+        Keyword arguments:
+        loc -- Grid location, Ex. 'A3'
+        """
         for battleship in self.players[1]['fleet']:
             if loc in battleship.pos:
                 return battleship
         return None
 
     def is_game_over(self):
+        """Sets game_over flag.
+        True, If all opposing players ships are sunk.
+        """
         sunken_ships = self.players[1]['sunk']
         total_ships = len(self.players[1]['fleet'])
         self.game_over = True if sunken_ships >= total_ships else False
@@ -126,15 +131,25 @@ class Game:
         print("\033c", end="")
 
     def prompt_to_continue(self, message):
+        """Prompts user to press ENTER with any given message.
+
+        Keyword arguments:
+        message -- a string message to be displayed
+        """
         input('{} {}'.format(message, 'Press ENTER to continue...'))
 
     def print_placement_header(self, player):
+        """Displays the ship placement header for a given player.
+
+        Keyword arguments:
+        player -- a Player()
+        """
         print('-' * 25, '\n')
         print('{} PLACE YOUR BATTLESHIPS\n'.format(player))
         print('-' * 25, '\n')
 
     def print_board_heading(self, board):
-        """Displays the heading section of the board"""
+        """Displays the heading section of the board."""
         print("   " + " ".join(board.board['heading']))
 
     def print_board(self, board):
@@ -151,6 +166,7 @@ class Game:
             row_num += 1
 
     def print_game_board(self):
+        """Displays both boards for current player."""
         print('-' * 25, '\n', 'Shots at Enemy\n', '-' * 25, '\n')
         print(self.print_board(self.players[0]['moves']))
         print('=' * 25)
@@ -158,6 +174,7 @@ class Game:
         print('-' * 25, '\nShots at You\n', '-' * 25)
 
     def initialize_players(self):
+        """Sets up initial state for all game players."""
         players = []
         for i in range(Game.MAX_PLAYERS):
             name = Player(self.get_player_name(i + 1))
